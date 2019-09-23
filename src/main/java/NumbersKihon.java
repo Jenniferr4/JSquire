ec84bd16dc310f83f07f493b8c9f514570b09d03
Jennier Granados <Jennier.Granados@improving.com>
2019-09-23 09:03:20 -0500

    changed morning drill

--- src/main/java/NumbersKihon.java
+++ src/main/java/NumbersKihon.java
@@ -3,36 +3,41 @@ import framework.NumbersKihonBase;
 public class NumbersKihon extends NumbersKihonBase {
     @Override
     protected int returnTheMaximumValueOfInt() {
-        throw new UnsupportedOperationException();
+        return Integer.MAX_VALUE;
     }
 
     @Override
     protected int returnTheMinimumValueOfInt() {
-        throw new UnsupportedOperationException();
+        return Integer.MIN_VALUE;
     }
 
     @Override
     protected int returnTheRemainderOfADividedByB(int a, int b) {
+
         throw new UnsupportedOperationException();
     }
 
     @Override
     protected double returnTheMaximumValueOfDouble() {
+
         throw new UnsupportedOperationException();
     }
 
     @Override
     protected double returnTheMinimumValueOfDouble() {
+
         throw new UnsupportedOperationException();
     }
 
     @Override
     protected boolean returnTrueIfAIsNotANumber(double a) {
+
         throw new UnsupportedOperationException();
     }
 
     @Override
     protected boolean returnTrueIfAIsAnInfinite(double a) {
+
         throw new UnsupportedOperationException();
     }
 }
